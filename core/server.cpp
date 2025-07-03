a8cdf7e3b26197d209054ca9b7a1e1e008b6d851
oriorori <junha3679@gmail.com>
2025-07-03 16:55:34 +0900

    add comment

--- core/server.cpp
+++ core/server.cpp
@@ -12,6 +12,20 @@ void Server::start_accept() {
         if (!ec) {
             std::make_shared<Session>(std::move(*socket))->start();
         }
+        // 원래는 this->start_accept();를 호출해야 하지만,
+        // 람다에서 this를 캡처하고 있으므로 start_accept()만 호출해도 this->start_accept()로 해석해준다.
         start_accept(); // 다음 클라이언트를 위해 반복
     });
 }
+
+/*
+acceptor_: TCP 소켓을 수신 대기하는 acceptor 객체입니다. 지정된 포트에서 클라이언트 연결을 기다립니다.
+start_accept(): 클라이언트 연결을 수락하기 위한 비동기 작업을 시작합니다. 클라이언트가 연결되면 새로운 세션을 생성하고 시작합니다.
+socket: 클라이언트와의 연결을 관리하는 소켓 객체입니다. 클라이언트가 연결되면 이 소켓을 사용하여 데이터를 송수신합니다.
+
+async_accept(): 비동기적으로 클라이언트 연결 요청을 처리합니다. 두번째 파라미터는 콜백 함수로, 클라이언트의 연결 요청 처리 이후 호출됩니다.
+이 때, 콜백은 연결 성공 여부와 관계없이 마지막에 무조건 실행됩니다.
+그래서 error code가 없을 때만 Session 객체를 생성하고 start 메서드를 호출하는 것을 확인할 수 있습니다.
+콜백 함수에 다시 start_accept()를 호출하여 다음 클라이언트 연결 요청을 기다립니다.
+
+*/
--- core/session.cpp
+++ core/session.cpp
@@ -8,20 +8,26 @@ void Session::start() {
     do_read();
 }
 
+// async_read_some: 클라이언트로부터 비동기적으로 데이터를 읽어오는 함수
 void Session::do_read() {
     auto self(shared_from_this());
     socket_.async_read_some(boost::asio::buffer(data_, 1024),
         [this, self](boost::system::error_code ec, std::size_t length) {
+            std::cout << "데이터 수신 완료: " << std::string(data_, length) << std::endl;
+
             if (!ec) {
                 do_write(length); // 받은 내용을 그대로 돌려줌 (Echo)
             }
         });
 }
 
+// async_write: 클라이언트에게 비동기적으로 데이터를 보내는 함수
 void Session::do_write(std::size_t length) {
     auto self(shared_from_this());
     boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
         [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+            std::cout << "데이터 전송 완료" << std::endl;
+
             if (!ec) {
                 do_read(); // 다시 읽기 대기
             }
